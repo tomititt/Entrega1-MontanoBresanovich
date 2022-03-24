1161e04f9fb23f8d06c0e837e3e69d29219a6857
Tomas Montano Bresanovich <monbretom@gmail.com>
2022-03-24 15:07:15 -0300

    Proyecto Final Coderhouse Iniciado

---
+++ appcoder/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Curso(models.Model):
+
+    
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField(primary_key=True)
+
+class Estudiante(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField(primary_key=True)
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField(primary_key=True)
+    profesion = models.CharField(max_length=60)
+
+class Entregable(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=40)
+    fecha_entregado = models.DateField()
+    entregado = models.BooleanField()
